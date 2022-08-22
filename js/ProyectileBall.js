7e925ffd560bcfa8f5cab0c5fa48bd5eb34f71f1
Jose Rios <jrbernabe@gmail.com>
2022-08-22 16:53:24 -0400

    Modify proyectile movement and add rebounds

--- js/ProyectileBall.js
+++ js/ProyectileBall.js
@@ -1,11 +1,21 @@
 import { Ball } from "./Ball.js";
 
 export class ProyectileBall extends Ball {
-  constructor(x, y, width, height, color, image, canvas, ctx) {
+  constructor(x, y, width, height, color, image, canvas, ctx, character) {
     super(x, y, width, height, color, image, canvas, ctx);
+    this.character = character;
+    this.speedX = 0;
+    this.speedY = 0;
   }
-  rebound() {}
-  compareColors() {}
-  deleteBalls() {}
-  move() {}
+  updateSpeed() {
+    this.speedX = (this.character.pointerX - this.canvas.width / 2);
+    this.speedY = -(this.character.pointerY - this.canvas.height - this.character.height / 2);
+  }
+  move() {
+    this.x += this.speedX / 50;
+    this.y -= this.speedY / 50;
+    if (this.x >= this.canvas.width - this.width / 2 || this.x <= this.width / 2) {
+      this.speedX *= -1;
+    }
+  }
 }
--- js/game.js
+++ js/game.js
@@ -7,7 +7,16 @@ const ctx = canvas.getContext('2d');
 
 const character1 = new Character((canvas.width - 50) / 2, canvas.height, 50, -100, '', '', 3, 0, canvas, ctx);
 // const character2 = new Character((canvas.width - 50) / 2, canvas.height, 50, -100, '', '', 3, 0, ctx);
-const proyectile = new ProyectileBall(canvas.width / 2, canvas.height + character1.height / 2, 50, 50, 'red', '', canvas, ctx);
+const proyectile = new ProyectileBall(canvas.width / 2, canvas.height + character1.height / 2, 50, 50, 'red', '', canvas, ctx, character1);
+
+const level1 = [
+  ['R','R','Y','Y','B','B','G','G'],
+  ['R','R','Y','Y','B','B','G'],
+  ['B','B','G','G','R','R','Y','Y'],
+  ['B','G','G','R','R','Y','Y']
+];
+const ballsGrid = [];
+
 function startGame() {
   detectKeysPressed(character1);
   let slope, x1, x2, y1, y2;
@@ -16,35 +25,18 @@ function startGame() {
     character1.draw();
     character1.drawAimAssist();
     // Drawing level
-    drawLevel();
+    drawLevel(level1);
     
     proyectile.draw();
     if(character1.shooting === true) {
-      // (y2 - y1) / (x2 - x1)  --> Slop (m)
-      // (y - y1) = (x - x1)m
-      if (
-        proyectile.x > proyectile.width / 2 &&
-        proyectile.x < canvas.width - proyectile.width / 2 &&
-        proyectile.y > proyectile.width / 2
-      ) {
-        x1 = canvas.width / 2;
-        x2 = character1.pointerX;
-        y1 = canvas.height + character1.height / 2;
-        y2 = character1.pointerY;
-        slope = (y2 - y1) / (x2 - x1);
-        proyectile.y -= 10;
-        if (slope === -Infinity) {
-          proyectile.x = x1;
-        } else {
-          proyectile.x = (proyectile.y - y1) / slope + x1;
-        }
-      } else {
-        character1.shooting = false;
-        proyectile.x = canvas.width / 2;
-        proyectile.y = canvas.height + character1.height / 2; 
+      if(proyectile.y > proyectile.width / 2) {
+        proyectile.move();
       }
+    } else {
+      proyectile.updateSpeed();
     }
-  }, 1000 / 30);
+  }, 1000 / 60);
+  console.log(ballsGrid)
 }
 
 function detectKeysPressed(character) {
@@ -67,35 +59,30 @@ function detectKeysPressed(character) {
   });
 }
 
-function drawLevel() {
-  const ballsGrid = [];
-  const ballsGridLayout = [
-    ['R','R','Y','Y','B','B','G','G'],
-    ['R','R','Y','Y','B','B','G'],
-    ['B','B','G','G','R','R','Y','Y'],
-    ['B','G','G','R','R','Y','Y']
-  ];
-  for (let i = 0; i < ballsGridLayout.length; i++) {
-      ballsGrid.push([]);
-    for (let j = 0; j < ballsGridLayout[i].length; j++) {
-      switch (ballsGridLayout[i][j]) {
-        case 'R':
-          ballsGridLayout[i][j] = 'red';
-          break;
-        case 'Y':
-          ballsGridLayout[i][j] = 'yellow';
-          break;
-        case 'B':
-          ballsGridLayout[i][j] = 'blue';
-          break;
-        case 'G':
-          ballsGridLayout[i][j] = 'green';
-          break;
-      }
-      if (i % 2) {
-        ballsGrid[i].push(new EnemyBall((j * 50) + 50, (i * 50) + 25, 50, 50, ballsGridLayout[i][j], '', canvas, ctx));
-      } else {
-        ballsGrid[i].push(new EnemyBall((j * 50) + 25, (i * 50) + 25, 50, 50, ballsGridLayout[i][j], '', canvas, ctx));
+function drawLevel(level) {
+  if (ballsGrid.length < level.length){
+    for (let i = 0; i < level.length; i++) {
+        ballsGrid.push([]);
+      for (let j = 0; j < level[i].length; j++) {
+        switch (level[i][j]) {
+          case 'R':
+            level[i][j] = 'red';
+            break;
+          case 'Y':
+            level[i][j] = 'yellow';
+            break;
+          case 'B':
+            level[i][j] = 'blue';
+            break;
+          case 'G':
+            level[i][j] = 'green';
+            break;
+        }
+        if (i % 2) {
+          ballsGrid[i].push(new EnemyBall((j * 50) + 50, (i * 50) + 25, 50, 50, level[i][j], '', canvas, ctx));
+        } else {
+          ballsGrid[i].push(new EnemyBall((j * 50) + 25, (i * 50) + 25, 50, 50, level[i][j], '', canvas, ctx));
+        }
       }
     }
   }
@@ -107,4 +94,3 @@ function drawLevel() {
 }
 
 startGame();
-drawLevel();
